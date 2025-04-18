42f2764fcb0ba9f0ecf9663b12c6a092f1a6ed20
rayxona <rayxona@gmail.com>
2025-04-18 10:10:00 +0500

    Implement code changes to enhance functionality and improve performance

--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -1,7 +1,32 @@
+import { Link } from "react-router-dom";
 import style from "./register.module.scss";
 
 function Register() {
-  return <div></div>;
+  return (
+    <section className={style.wrapper}>
+      <div className={style.left}>
+        <img src="../public/sidebar.png" alt="Illustration" />
+      </div>
+      <div className={style.right}>
+        <form className={style.form}>
+          <h2>Login</h2>
+          <label>Email</label>
+          <input type="email" />
+          <label>Password</label>
+          <input type="password" />
+          <label>Create password</label>
+          <input type="password" />
+
+          <button type="submit">Create account</button>
+
+          <p className={style.footer}>
+            Already have an account?
+            <Link to="/login"> Login</Link>
+          </p>
+        </form>
+      </div>
+    </section>
+  );
 }
 
 export default Register;
