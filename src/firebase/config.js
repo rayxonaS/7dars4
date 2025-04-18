cad6449b826e8b4b175b7f1f7c885109a7c88318
rayxona <rayxona@gmail.com>
2025-04-18 10:28:44 +0500

    fix: update image path in Login component for correct rendering feat: add Firebase configuration for app initialization style: format Vite config file for consistency

---
+++ src/firebase/config.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCrqZeYIBo0HZUMxfWjJ8A_xaFPTF9tShE",
+  authDomain: "finance-c07da.firebaseapp.com",
+  projectId: "finance-c07da",
+  storageBucket: "finance-c07da.firebasestorage.app",
+  messagingSenderId: "173572209073",
+  appId: "1:173572209073:web:9407b090270814550c6590",
+};
+
+const app = initializeApp(firebaseConfig);
