c4123b7aa91d982213a9c8fb850b758f7d381117
rayxona <rayxona@gmail.com>
2025-04-23 10:01:03 +0500

    fix: handle potential null value from useCollectionsData in Overview component

--- src/pages/overview/Overview.jsx
+++ src/pages/overview/Overview.jsx
@@ -2,7 +2,7 @@ import { useCollectionsData } from "../../hooks/useCollectionsData";
 import style from "./overview.module.scss";
 
 function Overview() {
-  const { data, isPending } = useCollectionsData();
+  const { data, isPending } = useCollectionsData() || {};
   console.log(data);
   return <div>Overview</div>;
 }
