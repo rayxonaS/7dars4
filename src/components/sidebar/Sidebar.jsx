268d639075d19c70c437f2e62a7812f97d1f66e4
rayxona <rayxona@gmail.com>
2025-04-28 10:09:32 +0500

    feat: enhance Sidebar component with responsive link text and adjust layout styles in resets

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -8,64 +8,136 @@ function Sidebar({ showSidebar, setShowSidebar }) {
     }`}
     >
       <div>
-        <img src="../images/logo-large.svg" alt="" width={121} />
-        <Link className={style.link} to="/overview">
-          <img
-            className={style.header_image}
-            src="../images/icon-nav-overview.svg"
-            alt=""
-            width={24}
-          />
-          <span className={style.link_text}>Overview</span>
-        </Link>
-        <Link className={style.link} to="/transactions">
-          <img
-            className={style.header_image}
-            src="../images/icon-nav-transactions.svg"
-            alt=""
-            width={24}
-          />
-          <span className={style.link_text}>Transactions</span>
-        </Link>
-        <Link className={style.link} to="/budgets">
-          <img
-            className={style.header_image}
-            src="../images/icon-nav-budgets.svg"
-            alt=""
-            width={24}
-          />
-          <span className={style.link_text}>Budgets</span>
-        </Link>
-        <Link className={style.link} to="/pots">
-          <img
-            className={style.header_image}
-            src="../images/icon-nav-pots.svg"
-            alt=""
-            width={24}
-          />
-          <span className={style.link_text}>Pots</span>
-        </Link>
-        <Link className={style.link} to="/recuringBills">
-          <img
-            className={style.header_image}
-            src="../images/icon-nav-recurring-bills.svg"
-            alt=""
-            width={24}
-          />
-          <span className={style.link_text}>RecuringBills</span>
-        </Link>
+        {showSidebar ? (
+          <img src="../images/logo-small.svg" alt="" width={13} />
+        ) : (
+          <img src="../images/logo-large.svg" alt="" width={121} />
+        )}
+        {showSidebar ? (
+          <Link className={style.link} to="/overview">
+            <img
+              className={style.header_image}
+              src="../images/icon-nav-overview.svg"
+              alt=""
+              width={24}
+            />
+          </Link>
+        ) : (
+          <Link className={style.link} to="/overview">
+            <img
+              className={style.header_image}
+              src="../images/icon-nav-overview.svg"
+              alt=""
+              width={24}
+            />
+            Overview
+          </Link>
+        )}
+        {showSidebar ? (
+          <Link className={style.link} to="/transactions">
+            <img
+              className={style.header_image}
+              src="../images/icon-nav-transactions.svg"
+              alt=""
+              width={24}
+            />
+          </Link>
+        ) : (
+          <Link className={style.link} to="/transactions">
+            <img
+              className={style.header_image}
+              src="../images/icon-nav-transactions.svg"
+              alt=""
+              width={24}
+            />
+            Transactions
+          </Link>
+        )}
+        {showSidebar ? (
+          <Link className={style.link} to="/budgets">
+            <img
+              className={style.header_image}
+              src="../images/icon-nav-budgets.svg"
+              alt=""
+              width={24}
+            />
+          </Link>
+        ) : (
+          <Link className={style.link} to="/budgets">
+            <img
+              className={style.header_image}
+              src="../images/icon-nav-budgets.svg"
+              alt=""
+              width={24}
+            />
+            Budgets
+          </Link>
+        )}
+        {showSidebar ? (
+          <Link className={style.link} to="/pots">
+            <img
+              className={style.header_image}
+              src="../images/icon-nav-pots.svg"
+              alt=""
+              width={24}
+            />
+          </Link>
+        ) : (
+          <Link className={style.link} to="/pots">
+            <img
+              className={style.header_image}
+              src="../images/icon-nav-pots.svg"
+              alt=""
+              width={24}
+            />
+            Pots
+          </Link>
+        )}
+        {showSidebar ? (
+          <Link className={style.link} to="/recuringBills">
+            <img
+              className={style.header_image}
+              src="../images/icon-nav-recurring-bills.svg"
+              alt=""
+              width={24}
+            />
+          </Link>
+        ) : (
+          <Link className={style.link} to="/recuringBills">
+            <img
+              className={style.header_image}
+              src="../images/icon-nav-recurring-bills.svg"
+              alt=""
+              width={24}
+            />
+            RecuringBills
+          </Link>
+        )}
       </div>
-      <button
-        onClick={() => setShowSidebar(!showSidebar)}
-        className={style.button}
-      >
-        <img
-          className={style.button_image}
-          src="../images/icon-minimize-menu.svg"
-          alt=""
-        />
-        <span className={style.link_text}>Minimaze Menu</span>
-      </button>
+      {showSidebar ? (
+        <button
+          onClick={() => setShowSidebar(!showSidebar)}
+          className={style.button}
+        >
+          <img
+            className={style.button_image}
+            src="../images/icon-minimize-menu.svg"
+            alt=""
+          />
+        </button>
+      ) : (
+        <button
+          onClick={() => setShowSidebar(!showSidebar)}
+          className={style.button}
+        >
+          <img
+            className={style.button_image}
+            src="../images/icon-minimize-menu.svg"
+            alt=""
+          />
+          Minimaze Menu
+        </button>
+      )}
     </div>
   );
 }
